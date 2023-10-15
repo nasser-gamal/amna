bbe8cc8321dfc54d7e0b98cb4864f7c5fea519e4
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-10-15 06:29:19 +0300

    new edits

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,3 +31,31 @@ var swiper = new Swiper('.support-company-swiper', {
     },
   },
 });
+
+
+
+// Home Counter 
+let statsSection = document.querySelector('.stats');
+let nums = document.querySelectorAll('.stats .counter');
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop - 500) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+
